4cc0069d69850047ba3d272c1709ed8da72cdf17
Joseph Lisee <jlisee@gmail.com>
2013-08-17 15:35:08 -0400

    Split up mega into test
    There are now separate test methods for each command.  We still are running
    the install command before each of them though.  Later we might find a way
    to make that simpler.

--- python/tests/fulltests.py
+++ python/tests/fulltests.py
@@ -26,9 +26,15 @@ class FullTests(unittest.TestCase):
         """
         Sets up a little test XPM environment
         """
+
+        # Create test repository
         cls.repo_dir = tempfile.mkdtemp(suffix = '-testing-xpm-repo')
 
         build_tree.create_test_repo(cls.repo_dir)
+
+        # Provide a path to the xpd
+        cls.hello_xpd = os.path.join(cls.repo_dir, 'tree', 'hello.xpd')
+
 
     @classmethod
     def tearDownClass(cls):
@@ -38,10 +44,14 @@ class FullTests(unittest.TestCase):
         if os.path.exists(cls.repo_dir):
            shutil.rmtree(cls.repo_dir)
 
+
     def setUp(self):
         # Create temp dir
         self.work_dir = tempfile.mkdtemp(suffix = '-testing-xpm')
         print self.work_dir
+
+        # Save the env_dir
+        self.env_dir = os.path.join(self.work_dir, 'env')
 
 
     def tearDown(self):
@@ -50,10 +60,13 @@ class FullTests(unittest.TestCase):
            shutil.rmtree(self.work_dir)
 
 
-    def _xpm_cmd(self, env_dir, args):
+    def _xpm_cmd(self, args, env_dir=None):
         """
         Run XPM command and return the output.
         """
+
+        if env_dir is None:
+            env_dir = self.env_dir
 
         cmd = [os.path.join(root_dir, 'xpm')] + args + [env_dir]
 
@@ -61,41 +74,61 @@ class FullTests(unittest.TestCase):
 
 
     def test_no_env(self):
+        """
+        Make sure we get an error when there is no environment.
+        """
 
         env_dir = os.path.join(self.work_dir, 'env')
 
-        output = self._xpm_cmd(env_dir, ['list'])
+        output = self._xpm_cmd(['list'])
 
         self.assertRegexpMatches(output, 'No XPM package DB found in root.*')
 
 
-    def test_everything(self):
-        # Get the paths to the env and the other versions
-        env_dir = os.path.join(self.work_dir, 'env')
-        package_xpd = os.path.join(self.repo_dir, 'tree', 'hello.xpd')
+    def test_install(self):
+        """
+        Make sure the desired program was installed and works.
+        """
 
         # Run the install
-        self._xpm_cmd(env_dir, ['install', package_xpd])
+        self._xpm_cmd(['install', self.hello_xpd])
 
         # Run our program to make sure it works
-        hello_bin = os.path.join(env_dir, 'bin', 'hello')
+        hello_bin = os.path.join(self.env_dir, 'bin', 'hello')
 
         output = util.shellcmd(hello_bin, echo=False)
 
         self.assertEqual('Hello, world!\n', output)
 
-        # Make sure the package is marked installed
-        output = self._xpm_cmd(env_dir, ['info', 'hello'])
+
+    def test_info(self):
+        """
+        Makes sure we can get the proper information back about an installed
+        package.
+        """
+        # Install the package
+        self._xpm_cmd(['install', self.hello_xpd])
+
+        # Make sure the info command returns the right data
+        output = self._xpm_cmd(['info', 'hello'])
 
         self.assertEqual('Package hello at version 1.0.0\n', output)
 
+
+    def test_remove(self):
+
+        # Install the package
+        self._xpm_cmd(['install', self.hello_xpd])
+
         # Get the package list
-        output = self._xpm_cmd(env_dir, ['list'])
+        output = self._xpm_cmd(['list'])
 
         self.assertEqual('  hello - 1.0.0\n', output)
 
         # Un-install the package
-        self._xpm_cmd(env_dir, ['remove', 'hello'])
+        self._xpm_cmd(['remove', 'hello'])
+
+        hello_bin = os.path.join(self.env_dir, 'bin', 'hello')
 
         self.assertFalse(os.path.exists(hello_bin))
 
