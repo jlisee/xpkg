bfc73b0d520a08f887a5dfb1ca4e4311517fdcc2
Joseph Lisee <jlisee@gmail.com>
2013-09-20 22:42:12 -0400

    greeter test app prints install dir
    The install directory is embedded in the binary of libgreet at build time.
    We want to change these paths when we install binary packages, and this is
    our test case.

--- tests/greeter/greeter.cpp
+++ tests/greeter/greeter.cpp
@@ -1,8 +1,22 @@
+#include <string>
+
 #include "greet/greet.h"
 
 int main(int argc, char** argv)
 {
-  greet::sayhello();
+  bool sayinstall = false;
+
+  if ((argc == 2) && (std::string("-i") == std::string(argv[1])))
+      sayinstall = true;
+
+  if (sayinstall)
+  {
+    greet::sayinstall();
+  }
+  else
+  {
+    greet::sayhello();
+  }
 
   return 0;
 }
--- tests/libgreet/greet.h
+++ tests/libgreet/greet.h
@@ -3,7 +3,11 @@
 
 namespace greet {
 
+// Prints a basic creating
 void sayhello();
+
+// Says hello from it's install location (embedded at build time)
+void sayinstall();
 
 }
 
