e7a9834b491907ad05d04deb90a039b822106969
Joseph Lisee <jlisee@gmail.com>
2013-10-05 14:28:54 -0400

    Update info command to tell us about our environment
    This is to make the environment setup less opaque to the end user.  Just
    run the command then find all the sources of the data you are using and
    where things will be put.

--- python/xpkg/main.py
+++ python/xpkg/main.py
@@ -87,7 +87,15 @@ def info(args):
     input_val = args.name
 
 
-    if input_val.endswith('.xpa'):
+    if input_val is None:
+        # Nothing supplied, grab environment and dump information about it
+        env = _create_env(args.root)
+
+        _environment_info(env)
+
+        info = None
+
+    elif input_val.endswith('.xpa'):
         # Actual package parse and print out information
         xpa = core.XPA(input_val)
 
@@ -102,28 +110,47 @@ def info(args):
 
     # Display info
     if info:
-        print '  name:',info['name']
-        print '  version:',info['version']
-        if info.get('description'):
-            description = info['description'].strip()
-
-            # Process extra long descriptions
-            if (80 - len('  description: ')) < len(description):
-                description = util.wrap_yaml_string(description)
-
-            print '  description:',description
-        if info.get('dependencies', None):
-            print '  dependencies:',info['dependencies']
-
-        if args.verbose:
-            print '  dirs:'
-            for f in sorted(info['dirs']):
-                print '    -',f
-            print '  files:'
-            for f in sorted(info['files']):
-                print '    -',f
-    else:
+        _package_info(info, args.verbose)
+    elif not input_val is None:
         print 'Package %s not installed.' % input_val
+
+
+def _package_info(info, verbose):
+    """
+    Print package info a yaml like format.
+    """
+
+    print '  name:',info['name']
+    print '  version:',info['version']
+
+    if info.get('description'):
+        description = info['description'].strip()
+
+        # Process extra long descriptions
+        if (80 - len('  description: ')) < len(description):
+            description = util.wrap_yaml_string(description)
+        print '  description:',description
+
+    if info.get('dependencies', None):
+        print '  dependencies:',info['dependencies']
+
+    if verbose:
+        print '  dirs:'
+        for f in sorted(info['dirs']):
+            print '    -',f
+        print '  files:'
+        for f in sorted(info['files']):
+            print '    -',f
+
+
+def _environment_info(env):
+    """
+    Print information about the environment.
+    """
+
+    print '  root:',env.root
+    print '  tree:',env.tree_path
+    print '  repo:',env.repo_path
 
 
 def list_packages(args):
@@ -135,7 +162,6 @@ def list_packages(args):
     env = _create_env(args.root)
 
     # List packages
-
     for package, info in env._pdb.iter_packages():
         print '  %s - %s' % (package, info['version'])
 
@@ -201,7 +227,7 @@ def main(argv = None):
     parser_i.set_defaults(func=remove)
 
     parser_i = subparsers.add_parser('info', help=info.__doc__)
-    parser_i.add_argument('name', type=str, help='Name of package')
+    parser_i.add_argument('name', type=str, help='Name of package', nargs='?')
     parser_i.add_argument('-v','--verbose', action='store_true', default=False,
                           help='Show more details about the package')
     parser_i.add_argument(*root_args, **root_kwargs)
